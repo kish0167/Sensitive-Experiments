f1217f5895f55e48eecdfc3fded3b41eac951109
kish <simerkish@gmail.com>
2025-02-18 16:28:23 +0300

    add Harmonia

--- SensitiveExperiments/SensitiveExperimentsAddon.cs
+++ SensitiveExperiments/SensitiveExperimentsAddon.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using HarmonyLib;
+﻿using HarmonyLib;
 using UnityEngine;
-using Unity;
 
 namespace SensitiveExperiments
 {
@@ -14,20 +12,42 @@ namespace SensitiveExperiments
 
 		#endregion
 
+		#region Unity lifecycle
+
 		private void Start()
 		{
 			_harmony = new Harmony("com.KSP.SensitiveExperiments");
 			_harmony.PatchAll();
-		}
-
-		private void Update()
-		{
-			
+			GameEvents.OnExperimentDeployed.Add(ExperimentDeployedCallback);
 		}
 
 		private void OnDestroy()
 		{
 			_harmony.UnpatchAll();
 		}
+
+		#endregion
+
+		#region Private methods
+
+		private void ExperimentDeployedCallback(ScienceData data)
+		{
+			data.scienceValueRatio += 400;
+			data.labValue += 500;
+			data.baseTransmitValue += 600;
+			data.transmitBonus += 700;
+			data.dataAmount += 300;
+			data.scienceValueRatio = 1;
+			Debug.LogError("science done!");
+		}
+
+		#endregion
+
+		#region Local data
+
+		[HarmonyPatch(typeof(ModuleScienceExperiment))]
+		private class Patch { }
+
+		#endregion
 	}
 }
