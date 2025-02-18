3f7c5ec46181deb9cca5911fea3a9159a2ade3b7
kish <simerkish@gmail.com>
2025-02-18 08:37:39 +0300

    project setup

---
+++ SensitiveExperiments/SensitiveExperimentsAddon.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using HarmonyLib;
+using UnityEngine;
+using Unity;
+
+namespace SensitiveExperiments
+{
+	[KSPAddon(KSPAddon.Startup.Flight, false)]
+	public class SensitiveExperimentsAddon : MonoBehaviour
+	{
+		#region Variables
+
+		private Harmony _harmony;
+
+		#endregion
+
+		private void Start()
+		{
+			_harmony = new Harmony("com.KSP.SensitiveExperiments");
+			_harmony.PatchAll();
+		}
+
+		private void Update()
+		{
+			
+		}
+
+		private void OnDestroy()
+		{
+			_harmony.UnpatchAll();
+		}
+	}
+}
