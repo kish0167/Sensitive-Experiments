304b6d87348f8c07e211f2d3ff74a7c383c31a76
kish0167 <simerkish@gmail.com>
2025-02-22 13:04:02 +0300

    removed local references

--- SensitiveExperiments/SensitiveExperimentsAddon.cs
+++ SensitiveExperiments/SensitiveExperimentsAddon.cs
@@ -52,54 +52,6 @@ namespace SensitiveExperiments
 			#endregion
 		}
 
-		[HarmonyPatch(typeof(ResearchAndDevelopment), nameof(ResearchAndDevelopment.GetSubjectValue))]
-		private static class GetSubjectValuePatch
-		{
-			#region Private methods
-
-			private static void Postfix(ref float __result)
-			{
-				Debug.LogError($"{nameof(ResearchAndDevelopment.GetSubjectValue)} returned {__result}");
-			}
-
-			#endregion
-		}
-		
-		[HarmonyPatch(typeof(ResearchAndDevelopment), nameof(ResearchAndDevelopment.GetNextScienceValue))]
-		private static class GetNextScienceValuePatch
-		{
-			#region Private methods
-
-			private static void Postfix(ref float __result)
-			{
-				Debug.LogError($"{nameof(ResearchAndDevelopment.GetNextScienceValue)} returned {__result}");
-			}
-
-			#endregion
-		}
-		
-		[HarmonyPatch(typeof(ResearchAndDevelopment), nameof(ResearchAndDevelopment.GetExperimentSubject))]
-		private static class GetExperimentSubjectPatch
-		{
-			#region Private methods
-
-			private static void Postfix(ScienceSubject __result)
-			{
-				/*Debug.LogError($"{nameof(ResearchAndDevelopment.GetExperimentSubject)} returned ScienceSubject\n" +
-				               $"subjectValue: {__result.subjectValue}\n" +
-				               $"scientificValue: {__result.scientificValue}\n" +
-				               $"applyScienceScale: {__result.applyScienceScale}\n" +
-				               $"dataScale: {__result.dataScale}\n" +
-				               $"scienceCap: {__result.scienceCap}\n" +
-				               $"title: {__result.title}\n" +
-				               $"science: {__result.science}\n");*/
-				
-			}
-
-			#endregion
-		}
-		
-
 		#endregion
 	}
 }
